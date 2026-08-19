3d504b5cf39fe20f357d1d8fe000a7d35c0441aa
Akula Akshay <akshayakula13@gmail.com>
2026-08-19 08:57:51 +0530

    terraform

---
+++ ec2/ec2.tf
@@ -0,0 +1,35 @@
+resource "aws_instance" "terraform" {
+    #key, value --> are nothing but arguments
+  ami           = "ami-0220d79f3f480ecf5"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow-all.id]
+
+  tags = {
+    Name = "Terraform control"
+    Terraform = "true"
+  }
+
+}
+
+resource "aws_security_group" "allow-all" { #allow-all --> for refering this in another resource in tf; tf ref
+  name = "allow-all-sg" #allow-all-sg --> security group name
+
+  egress { #outgoing traffic
+    from_port       = 0 #from_port 0 to to_port 0 means all ports
+    to_port         = 0
+    protocol        = "-1" #-1 means all protocols
+    cidr_blocks = ["0.0.0.0/0"] #0.0.0.0/0 means internet
+  }
+
+  ingress { #incomming traffic
+    from_port       = 0 #from_port 0 to to_port 0 means all ports
+    to_port         = 0
+    protocol        = "-1" #-1 means all protocols
+    cidr_blocks = ["0.0.0.0/0"] #0.0.0.0/0 --> means internet
+  }
+
+  tags = {
+    Name = "Terraform-sg" #name --> user display
+  }
+
+}
